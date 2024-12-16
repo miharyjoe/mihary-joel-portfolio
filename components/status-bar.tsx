944a75fa57e2487c61b7fbf57991a009a0a2bd80
miharyjoe <miharyjoel@gmail.com>
2024-12-16 22:24:34 +0300

    fix: responsive and logo

--- components/status-bar.tsx
+++ components/status-bar.tsx
@@ -1,15 +1,31 @@
+import { CheckCheckIcon, GitBranch } from "lucide-react";
+
 export function StatusBar() {
   return (
-    <div className="h-6 border-t flex items-center px-2 text-xs text-muted-foreground justify-between">
-      <div className="flex items-center gap-2">
-        <span>L 1, col 1</span>
-        <span>Espaces : 2</span>
+    <div className="h-6 border-t flex flex-wrap items-center px-2 text-xs text-muted-foreground justify-between bg-accent/30">
+      <div className="flex items-center gap-4">
+        <div className="flex items-center gap-2">
+          <span className="flex items-center gap-1">
+            <span className=" border border-blue-400 rounded px-1 py-0.5 bg-blue-400 ">
+              Mihary Joel
+            </span>
+          </span>
+          <span className="flex items-center gap-1">
+            <GitBranch className="h-4 w-4" />
+            <span>prod</span>
+          </span>
+        </div>
+      </div>
+      <div className="flex items-center gap-4 flex-wrap">
+        <span className="hidden sm:inline">L 3, col 24</span>
+        <span className="hidden sm:inline">Espaces : 2</span>
         <span>UTF-8</span>
         <span>LF</span>
-        <span>HTML</span>
-      </div>
-      <div className="flex items-center gap-2">
-        <span>Disposition : French</span>
+        <span className="hidden sm:inline">TypeScript JSX</span>
+        <span className="flex gap-1 hover:bg-accent/50 px-1 rounded-sm">
+          <CheckCheckIcon className="w-4 h-4" />
+          <span>Prettier</span>
+        </span>
       </div>
     </div>
   );
