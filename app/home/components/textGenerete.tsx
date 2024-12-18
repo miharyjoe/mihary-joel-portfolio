bdc04383847fd574a637372ce004cda945020dbf
miharyjoe <miharyjoel@gmail.com>
2024-12-18 23:01:41 +0300

    feat: add test generate effect

---
+++ app/home/components/textGenerete.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { TextGenerateEffect } from "@/components/ui/text-generate-effect";
+
+const words = `Keep learning new lines of code, and you’ll keep creating new possibilities.`;
+
+export function TextGenerateEffectDemo() {
+  return <TextGenerateEffect words={words} />;
+}
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,17 +1,27 @@
 import { BackgroundLines } from "@/components/ui/background-lines";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { TextGenerateEffectDemo } from "./components/textGenerete";
 
 export default function Page() {
   return (
-    <BackgroundLines className="flex items-center justify-center w-full flex-col px-4">
+    <BackgroundLines className="flex items-center justify-center w-full flex-col px-4 ">
       <h2 className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
         Hi there !! <br /> I'm Mihary Joël <br /> Andriamilanto.
       </h2>
       <p className="max-w-xl mx-auto text-sm md:text-lg text-neutral-700 dark:text-neutral-400 text-center">
         Fullstack web developer 🌐
       </p>
+      <div className="max-w-xl mx-auto text-sm md:text-lg text-neutral-700 dark:text-neutral-400 text-center">
+        <TextGenerateEffectDemo />
+      </div>
       <div className="flex items-center justify-center gap-4 mt-4">
-        <Button>About me </Button>
+        <Button variant={"secondary"} size={"lg"}>
+          <Link href="/about">About me</Link>
+        </Button>
+        <Button variant={"destructive"} size={"lg"}>
+          <Link href="/project">My project</Link>
+        </Button>
       </div>
     </BackgroundLines>
   );
