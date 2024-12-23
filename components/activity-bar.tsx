4ef9708fa2b366e30e7d2cd54dcf07c94b77b631
miharyjoe <miharyjoel@gmail.com>
2024-12-23 21:28:21 +0300

    feat: add nav link

--- components/activity-bar.tsx
+++ components/activity-bar.tsx
@@ -6,21 +6,31 @@ export function ActivityBar() {
     <div className="w-12 border-r flex flex-col justify-between py-2">
       <div className="flex flex-col items-center gap-2">
         <button className="p-2 hover:bg-accent rounded-md text-accent-foreground/60 hover:text-accent-foreground">
-          <Files className="h-5 w-5" />
+          <Link href="/home">
+            <Files className="h-5 w-5" />
+          </Link>
         </button>
         <button className="p-2 hover:bg-accent rounded-md text-accent-foreground/60 hover:text-accent-foreground">
-          <Search className="h-5 w-5" />
+          <Link href="/project">
+            <Search className="h-5 w-5" />
+          </Link>
         </button>
         <button className="p-2 hover:bg-accent rounded-md text-accent-foreground/60 hover:text-accent-foreground">
-          <GitBranch className="h-5 w-5" />
+          <Link href="http://github.com/miharyjoe" target="_blank">
+            <GitBranch className="h-5 w-5" />
+          </Link>
         </button>
         <button className="p-2 hover:bg-accent rounded-md text-accent-foreground/60 hover:text-accent-foreground">
-          <Box className="h-5 w-5" />
+          <Link href="/contact">
+            <Box className="h-5 w-5" />
+          </Link>
         </button>
       </div>
       <div className="flex flex-col items-center gap-2">
         <button className="p-2 hover:bg-accent rounded-md text-accent-foreground/60 hover:text-accent-foreground">
-          <User className="h-5 w-5" />
+          <Link href="/about">
+            <User className="h-5 w-5" />
+          </Link>
         </button>
         <button className="p-2 hover:bg-accent rounded-md text-accent-foreground/60 hover:text-accent-foreground">
           <Link href="/setting">
