d4269b10b9eb7263859e8fd6b7625469364c7e7b
miharyjoe <miharyjoel@gmail.com>
2024-12-23 16:30:02 +0300

    feat: add project list and update project card with image

---
+++ app/project/components/projectList.ts
@@ -0,0 +1,40 @@
+export const projectList = [
+  {
+    title: "Tips my life (Blog)",
+    description:
+      "A blog where I share my life tips and experiences. I write about productivity, personal development, and software engineering.",
+    projectUrl: "https://tipsmylife.live",
+    languages: ["TypeScript", "React", "Nextjs", "Tailwind_CSS", "mdx"],
+    category: "Full Stack",
+    date: "",
+    imageUrl: "/assets/tipsmylife2.jpeg",
+  },
+  {
+    title: "Image to Pdf",
+    description: "An API to convert images to pdf in spring boot",
+    projectUrl: "https://github.com/miharyjoe/Image-To-Pdf-Api",
+    languages: ["java", "Spring_boot"],
+    category: "Backend",
+    date: "",
+    imageUrl: "/assets/imageToPdf.jpeg",
+  },
+  {
+    title: "translator-ai-api",
+    description:
+      "This Spring Boot application provides endpoints for AI-assisted language processing, including spelling and grammar correction and text translation.",
+    projectUrl: "https://github.com/miharyjoe/Image-To-Pdf-Api",
+    languages: ["java", "Spring_boot", "chatGPT"],
+    category: "Backend",
+    date: "",
+    imageUrl: "/assets/translator.jpeg",
+  },
+  {
+    title: "Reddit clone Api",
+    description: "A reddit clone API using spring boot",
+    projectUrl: "https://github.com/miharyjoe/spring-reddit-clone",
+    languages: ["java", "Spring_boot"],
+    category: "Backend",
+    date: "",
+    imageUrl: "/assets/redditClone.jpeg",
+  },
+];
--- app/project/types/projet.ts
+++ app/project/types/projet.ts
@@ -5,4 +5,5 @@ export interface Project {
   languages: string[];
   category?: string;
   date?: string;
+  imageUrl?: string;
 }
